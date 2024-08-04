38817d53b6020c6d48a36fd8df6aa59b30d1464e
Yurkmez <yurkmez.shaikh@gmail.com>
2024-08-04 12:31:18 +0300

    (1) commit - developmen wıth Handlebars: layout, partials, ets.

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
+// ____ handlebars ________________
+//  Создаем и формируем параметры движка
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs',
+});
+app.engine('hbs', hbs.engine); // Регистрируем hbs в качестве движка для рендеринга HTML страниц
+app.set('view engine', 'hbs'); // Установки по умолчанию: название движка
+app.set('views', 'views'); // и директория, в которй будут размещены файлы для рендеринга
+// ___________________________________
 app.get('/', (req, res) => {
-    res.status(200);
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.render('index'); // Здесь мы просто указываем название файла, т.к. выше мы указали директорию views в параметрах по умолчанию
+});
+app.get('/about', (req, res) => {
+    res.render('about'); // - " -
 });
 
 const PORT = process.env.PORT || 5000;
